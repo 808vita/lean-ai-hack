2d9922cae31a76a691c82d733540d717de80d141
808vita <its808vita@gmail.com>
2025-04-28 07:40:57 +0530

    setup packages , folders

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["beeai-framework", "zod"],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
